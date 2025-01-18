c06ebdee93988da513c832078310c10052045a0b
applegrammer <web52995@gmail.com>
2025-01-18 17:25:48 +0100

    add monaco and xterm to dependencies

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+const Routes: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+
+export default Routes;
